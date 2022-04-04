6b977613b3146cb309c7ae5ff594c9887f19da27
Natalija Germek <n.germek@icloud.com>
2022-04-03 17:18:46 -0700

    added content to eslint and gitignore.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-'use strict'
+'use strict';
 
 console.log(`Hello World`);
 
-const hour = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.']
+let hour = ['6 a.m.', '7 a.m.', '8 a.m.', '9 a.m.', '10 a.m.', '11 a.m.', '12 p.m.', '1 p.m.', '2 p.m.', '3 p.m.', '4 p.m.', '5 p.m.', '6 p.m.', '7 p.m.']
 
 console.log(hour);
-console.log(hour[2]);
 
 let seattle = {
     name: 'Seattle',
@@ -24,22 +23,226 @@ let seattle = {
         for (let i = 0; i < hour.length; i++) {
             let customer = this.getRandomCustomer();
             let cookiesSold = Math.floor(customer * this.avg);
+            this.dailyTotal = (this.dailyTotal + cookiesSold);
             this.avgCookiesSoldHoulyArray.push(cookiesSold);
         }
     },
     sum: function () {
-            let totalHourlyCookies = 0;
-            //this is assigning multiple of any cookie index in the HOUR array.
-            for (let i = 0; i < this.avgCookiesSoldHoulyArray.length; i++) {
-                totalHourlyCookies += this.avgCookiesSoldHoulyArray[i];
-                // this is being multiplied and then reassigned
-                // if finding the letters in the "index" field, look back to the begining of the loop to see what vault it started at. That is the value it is starting at. 
-            }
-            let display = `The total cookies sold are ${totalHourlyCookies}.`;
-            console.log(display);
-            return [totalHourlyCookies, display];
+        let totalHourlyCookies = 0;
+        //this is assigning multiple of any cookie index in the HOUR array.
+        for (let i = 0; i < this.avgCookiesSoldHoulyArray.length; i++) {
+            totalHourlyCookies += this.avgCookiesSoldHoulyArray[i];
+            // this is being multiplied and then reassigned
+            // if finding the letters in the "index" field, look back to the begining of the loop to see what vault it started at. That is the value it is starting at. 
+        }
+        let display = `The total cookies sold are ${totalHourlyCookies}.`;
+        console.log(display);
+        return [totalHourlyCookies, display];
+    },
+    render: function () {
+        this.calcCookiesPerHour();
+        let seattleStore = document.getElementById('sea');
+        let seattleUl = document.createElement('ul');
+        console.log(this.avgCookiesSoldHoulyArray);
+        for (let j = 0; j < this.avgCookiesSoldHoulyArray.length; j++) {
+            let seaLi = document.createElement('li');
+            seaLi.textContent = `${hour[j]} ${this.avgCookiesSoldHoulyArray[j]}`;
+            seattleUl.appendChild(seaLi);
+        }
+        seattleStore.appendChild(seattleUl);
+    
+    let total = document.createElement('li');
+    total.textContent = `total ${this.dailyTotal}`;
+    seattleStore.appendChild(total)
     }
 }
-seattle.calcCookiesPerHour();
+// function hours() {
+//     let hoursArray = document.getElementById('hours');
+//     let hoursUl = document.createElement('ul');
+//     console.log(hour);
+//     for (let k = 0; k < hour.length; k++) {
+//         let timeLi = document.createElement('li');
+//         timeLi.textContent = hour[k];
+//         hoursUl.appendChild(timeLi);
+//     }
+//     hoursArray.appendChild(hoursUl);
+// }
+
+// hours();
+// seattle.calcCookiesPerHour();
 console.log(seattle);
 seattle.sum();
+seattle.render();
+
+let tokyo = {
+    name: 'Tokyo',
+    min: 3,
+    max: 24,
+    avg: 1.2,
+    dailyTotal: 0,
+    avgCookiesSoldHoulyArray: [],
+
+    getRandomCustomer: function () {
+        //this is specifically for the customers
+        return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);// <--- generating random customer.
+    },
+
+    calcCookiesPerHour: function () {
+        for (let i = 0; i < hour.length; i++) {
+            let customer = this.getRandomCustomer();
+            let cookiesSold = Math.floor(customer * this.avg);
+            this.avgCookiesSoldHoulyArray.push(cookiesSold);
+        }
+    },
+    sum: function () {
+        let totalHourlyCookies = 0;
+        //this is assigning multiple of any cookie index in the HOUR array.
+        for (let i = 0; i < this.avgCookiesSoldHoulyArray.length; i++) {
+            totalHourlyCookies += this.avgCookiesSoldHoulyArray[i];
+            // this is being multiplied and then reassigned
+            // if finding the letters in the "index" field, look back to the begining of the loop to see what vault it started at. That is the value it is starting at. 
+        }
+        let display = `The total cookies sold are ${totalHourlyCookies}.`;
+        console.log(display);
+        return [totalHourlyCookies, display];
+    },
+    render: function () {
+        this.calcCookiesPerHour();
+        let tokyoStore = document.getElementById('sea');
+        let tokyoUl = document.createElement('ul');
+        console.log(this.avgCookiesSoldHoulyArray);
+        for (let j = 0; j < this.avgCookiesSoldHoulyArray.length; j++) {
+            let tokLi = document.createElement('li');
+            tokLi.textContent = `${hour[j]} ${this.avgCookiesSoldHoulyArray[j]}`;
+            tokyoUl.appendChild(tokLi);
+        }
+        tokyoStore.appendChild(tokyoUl);
+    
+    let total = document.createElement('li');
+    total.textContent = `total ${this.dailyTotal}`;
+    tokyoStore.appendChild(total)
+    }
+}
+// tokyo.calcCookiesPerHour();
+// console.log(tokyo);
+tokyo.sum();
+tokyo.render();
+
+let dubai = {
+    name: 'Dubai',
+    min: 11,
+    max: 38,
+    avg: 3.7,
+    dailyTotal: 0,
+    avgCookiesSoldHoulyArray: [],
+
+    getRandomCustomer: function () {
+        //this is specifically for the customers
+        return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);// <--- generating random customer.
+    },
+
+    calcCookiesPerHour: function () {
+        for (let i = 0; i < hour.length; i++) {
+            let customer = this.getRandomCustomer();
+            let cookiesSold = Math.floor(customer * this.avg);
+            this.avgCookiesSoldHoulyArray.push(cookiesSold);//<----- generating cookies per hour.
+        }
+    },
+    sum: function () {
+        let totalHourlyCookies = 0;
+        for (let i = 0; i < this.avgCookiesSoldHoulyArray.length; i++) {
+            totalHourlyCookies += this.avgCookiesSoldHoulyArray[i];
+            // this is being multiplied and then reassigned
+            // if finding the letters in the "index" field, look back to the begining of the loop, that is the value it is starting at. 
+        }
+        let display = `The total cookies sold are ${totalHourlyCookies}.`;
+        console.log(display);
+        return [totalHourlyCookies, display];//<------ calculating and returning totals of total cookies sold per store.
+    }
+}
+dubai.calcCookiesPerHour();
+// console.log(dubai);
+dubai.sum();
+
+//***** Do not forget! We have the key:let, we are assigning the name:paris and giving a value:all of the information from Paris. see formula below. ********//
+//key name: value //
+let paris = {//<--- naming the store with new parameters
+    name: 'Paris',
+    min: 23,
+    max: 65,
+    avg: 6.3,
+    dailyTotal: 0,
+    avgCookiesSoldHoulyArray: [],
+
+    getRandomCustomer: function () {
+        //this is specifically for the customers
+        return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);// <--- generating random customer.
+    },
+
+    calcCookiesPerHour: function () {
+        for (let i = 0; i < hour.length; i++) {
+            let customer = this.getRandomCustomer();
+            let cookiesSold = Math.floor(customer * this.avg);
+            this.avgCookiesSoldHoulyArray.push(cookiesSold);//<---generating cookies sold per hour
+        }
+    },
+    sum: function () {
+        let totalHourlyCookies = 0;
+        //this is assigning multiple of any cookie index in the HOUR array.
+        for (let i = 0; i < this.avgCookiesSoldHoulyArray.length; i++) {
+            totalHourlyCookies += this.avgCookiesSoldHoulyArray[i];
+            // if finding the letters in the "index" field, look back to the begining of the loop, that is the value it is starting at. 
+        }
+        let display = `The total cookies sold are ${totalHourlyCookies}.`;
+        console.log(display);
+        return [totalHourlyCookies, display];
+    }
+}
+paris.calcCookiesPerHour();
+// console.log(paris);
+paris.sum();
+
+let lima = {
+    name: 'Lima',
+    min: 2,
+    max: 16,
+    avg: 4.6,
+    dailyTotal: 0,
+    avgCookiesSoldHoulyArray: [],
+
+    getRandomCustomer: function () {
+        //this is specifically for the customers
+        return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);// <--- generating random customer.
+    },
+
+    calcCookiesPerHour: function () {
+        for (let i = 0; i < hour.length; i++) {
+            let customer = this.getRandomCustomer();
+            let cookiesSold = Math.floor(customer * this.avg);
+            this.avgCookiesSoldHoulyArray.push(cookiesSold);//<---generating random cookies sold.
+        }
+    },
+    sum: function () {
+        let totalHourlyCookies = 0;
+        //this is assigning multiple of any cookie index in the HOUR array.
+        for (let i = 0; i < this.avgCookiesSoldHoulyArray.length; i++) {
+            totalHourlyCookies += this.avgCookiesSoldHoulyArray[i];
+            // if finding the letters in the "index" field, look back to the begining of the loop, that is the value it is starting at. 
+        }
+        let display = `The total cookies sold are ${totalHourlyCookies}.`;
+        console.log(display);
+        return [totalHourlyCookies, display];
+    }
+}
+lima.calcCookiesPerHour();
+// console.log(lima);
+lima.sum();
+
+// let totalStoreSales: function (){
+//     let totalSales = 0;
+
+// }
+
+//grandTotalSales.sum();
+// let stores = document.createElement(ul);
+// let storeHours = document.createElement(li);
